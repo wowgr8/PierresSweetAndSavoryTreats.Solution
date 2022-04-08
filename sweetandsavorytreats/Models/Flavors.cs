d02ff285b77be385c262d24cf18c2e16a2149116
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-08 09:20:55 -0700

    add using directive, defined namespace, joinentities hashset for FlavorTreat jointable, and properties

---
+++ sweetandsavorytreats/Models/Flavors.cs
@@ -0,0 +1,16 @@
+using System.Collenctions.Generic;
+
+namespace SweetAndSavoryTreats.Models
+{
+  public class Flavor
+  {
+    public Flavor()
+    {
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+
+    public int FlavorId { get; set; }
+    public string FlavorName { get; set; }
+    public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+  }
+}
