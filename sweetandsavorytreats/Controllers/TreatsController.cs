9efd76592100ffd2add130cff1b4ca58137de84d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-08 11:54:59 -0700

    add using directives, namespace, and properties with 'Authorize' attribute. 'AllowAnonymous' to be added to certain functionalities soon. Added async Index and Create (get and async post) functionality

---
+++ sweetandsavorytreats/Controllers/TreatsController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SweetAndSavoryTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace SweetAndSavoryTreats.Controllers
+{
+  [Authorize]
+  public class TreatsController : Controller
+  {
+    private readonly SweetAndSavoryTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public RTreatController(UserManager<ApplicationUser> userManager, SweetAndSavoryTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userTreats);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat, int FlavorId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      _db.SaveChanges();
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
