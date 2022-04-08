893615838fa8508ed40b41c090d5bec8d617d0ef
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-08 08:27:26 -0700

    add application model for DesignTime

---
+++ sweetandsavorytreats/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SweetAndSavoryTreats.Models
+{
+  public class SweetAndSavoryTreatsContextFactory : IDesignTimeDbContextFactory<SweetAndSavoryTreatsContext>
+  {
+
+    SweetAndSavoryTreatsContext IDesignTimeDbContextFactory<SweetAndSavoryTreatsContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+      var builder = new DbContextOptionsBuilder<SweetAndSavoryTreatsContext>();
+
+      builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
+
+      return new RSweetAndSavoryTreatsontext(builder.Options);
+    }
+  }
+}
