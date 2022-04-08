a5377b5514490a5a46cfef0d49dc4eabc93f4eb2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-08 11:25:04 -0700

    add migration for Identity and Authorization

---
+++ sweetandsavorytreats/Migrations/20220408180610_IdentityAndAuthorization.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SweetAndSavoryTreats.Migrations
+{
+    public partial class IdentityAndAuthorization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
