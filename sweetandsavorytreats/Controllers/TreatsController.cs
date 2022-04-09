8cba5be136cf95d5df627335cd4d2c742cc4d85b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-08 21:41:49 -0700

    add fix to typo in controller, also added Details functionality as [AllowAnonymous] and Edit (get and post) functionality - Edit Post functionality also prevents duplicates from being created while editing

--- sweetandsavorytreats/Controllers/TreatsController.cs
+++ sweetandsavorytreats/Controllers/TreatsController.cs
@@ -17,7 +17,7 @@ namespace SweetAndSavoryTreats.Controllers
     private readonly SweetAndSavoryTreatsContext _db;
     private readonly UserManager<ApplicationUser> _userManager;
 
-    public RTreatController(UserManager<ApplicationUser> userManager, SweetAndSavoryTreatsContext db)
+    public TreatsController(UserManager<ApplicationUser> userManager, SweetAndSavoryTreatsContext db)
     {
       _userManager = userManager;
       _db = db;
@@ -48,6 +48,35 @@ namespace SweetAndSavoryTreats.Controllers
       {
         _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
       }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [AllowAnonymous]
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+        .Include(treat => treat.JoinEntities)
+        .ThenInclude(join => join.Flavor)
+        .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat, int FlavorId)
+    {
+      bool duplicate = _db.FlavorTreat.Any(join => join.FlavorId == FlavorId && join.TreatId == treat.TreatId);
+      if (FlavorId !=0 && !duplicate){
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.Entry(treat).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
