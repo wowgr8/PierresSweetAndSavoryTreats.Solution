7751df9a99d067901a56630f2224679eead2ce07
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-09 00:12:12 -0700

    add AddFlavor functionality (get and post)

--- sweetandsavorytreats/Controllers/TreatsController.cs
+++ sweetandsavorytreats/Controllers/TreatsController.cs
@@ -80,5 +80,23 @@ namespace SweetAndSavoryTreats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+      public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat=> treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+      _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
