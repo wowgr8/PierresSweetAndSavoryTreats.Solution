7663da44279e290e428e7352fea41a847881d84b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-10 00:57:31 -0700

    add fix to flavor name property

--- sweetandsavorytreats/Controllers/TreatsController.cs
+++ sweetandsavorytreats/Controllers/TreatsController.cs
@@ -32,7 +32,7 @@ namespace SweetAndSavoryTreats.Controllers
 
     public ActionResult Create()
     {
-      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
       return View();
     }
 
@@ -65,7 +65,7 @@ namespace SweetAndSavoryTreats.Controllers
     public ActionResult Edit(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
       return View(thisTreat);
     }
 
@@ -84,7 +84,7 @@ namespace SweetAndSavoryTreats.Controllers
     public ActionResult AddFlavor(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat=> treat.TreatId == id);
-      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
       return View(thisTreat);
     }
 
